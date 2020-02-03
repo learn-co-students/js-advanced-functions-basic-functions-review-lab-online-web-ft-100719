cc26993b869f7f2acca18f86a0a933162625e632
jacquibushfi <info@stainedglassofmckinney.com>
2020-02-03 09:51:13 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+function saturdayFun(fun = 'roller-skate') {
+  return `This Saturday, I want to ${fun}!`;
+}
+
+let mondayWork = function(fun = 'go to the office') {
+  return `This Monday, I will ${fun}.`;
+};
+
+let wrapAdjective = function(style = '*') {
+  return function(adjective = 'special') {
+    return `You are ${style}${adjective}${style}!`;
+  };
+};
+
+const Calculator = {
+  add: function(a, b) {
+    return a + b;
+  },
+  subtract: function(a, b) {
+    return a - b;
+  },
+  multiply: function(a, b) {
+    return a * b;
+  },
+  divide: function(a, b) {
+    return a / b;
+  },
+};
+
+let actionApplyer = function(start, ray) {
+  let a = start;
+  for (let i = 0; i < ray.length; i++) {
+    a = ray[i](a);
+  }
+  return a;
+};
